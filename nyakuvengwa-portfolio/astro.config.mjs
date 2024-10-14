d44a03100da799090ac16063b06688bc6d83212b
Arthur Moyo <30801176+Nyakuvengwa@users.noreply.github.com>
2024-10-14 20:13:00 +0200

    Other apps

--- nyakuvengwa-portfolio/astro.config.mjs
+++ nyakuvengwa-portfolio/astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://https://nyakuvengwa.github.io",
-    base: "/",
-    integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
--- nyakuvengwa-portfolio/src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Arthur Moyo | Github Page";
-export const SITE_DESCRIPTION =
-    "Welcome to my developer portfolio! Discover my collection of backend projects and APIs that showcase my expertise in building robust and scalable backend systems. With a passion for clean code and efficient algorithms, I strive to create backend solutions.";
